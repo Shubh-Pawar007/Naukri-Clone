a890eedc324c37abefd7529fb89af54d92691f35
Shubh-Pawar007 <shubhampawar242767@gmail.com>
2022-02-25 18:18:58 +0530

    Job Page update
    Functionality work remaining..

---
+++ JobPage/j1.js
@@ -0,0 +1,41 @@
+var data=JSON.parse(localStorage.getItem("jobDetail"));
+// console.log(data);
+
+var saveJobarr= localStorage.setItem("savedJob",JSON.stringify(data))||[]
+jobDataArr.map(function (elem){
+  var innerdiv= document.createElement("div");
+  innerdiv.setAttribute("class","innerdiv")
+  var p=document.createElement("h3");
+  p.innerText=elem.profile;
+  p.setAttribute("class","title")
+ 
+  var c= document.createElement("p");
+  c.innerText=elem.company;
+  //console.log(c);
+  var e= document.createElement("p");
+  e.innerText=elem.experience;
+  var s= document.createElement("p");
+  s.innerText=elem.salary;
+  var l= document.createElement("p");
+  l.innerText=elem.location;
+  var d= document.createElement("p");
+  d.innerText=elem.discription;
+  var btn=document.createElement("button");
+  btn.setAttribute("class","saveButton");
+  btn.innerText="Save the Job";
+  btn.addEventListener("click",function(){
+    saveJob(data);
+  })
+  innerdiv.append(p,c,e,s,l,d,btn);
+ 
+  document.querySelector("#jobs").append(innerdiv);
+
+  function saveJob(data){
+    console.log(data);
+    saveJobarr.push(data)
+    console.log(saveJobarr);
+    localStorage.setItem("savedJob",JSON.stringify(saveJobarr));
+  }
+
+})
+
