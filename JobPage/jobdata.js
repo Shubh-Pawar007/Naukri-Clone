b303dfa7f0fdc09f325303baa1930aabff2c9203
Shubh-Pawar007 <shubhampawar242767@gmail.com>
2022-02-26 19:08:13 +0530

    New Jobpage

---
+++ JobPage/jobdata.js
@@ -0,0 +1,56 @@
+
+var jobDataArr=[
+    //   Full Stack developer
+    {profile: "Full Stack Developer", company:"Wipro", experience:"1 - 3 years",salary:" ₹ 20L",location:"Mumbai Suburban, Thane, Mumbai (All Areas)",discription:"In-depth knowledge of Reactjs / Vue.JS/ Angular Good knowledge of HTML, HTML5, CSS, BOOTSTRAP, JAVASCRIPT & JQUERY."},
+     {profile: "Full Stack Developer", company:"Green Media Technology", experience:"1 - 3 years",salary:" 10L",location:"Mumbai Suburban, Thane, Mumbai (All Areas)",discription :"In-depth knowledge of Reactjs / Vue.JS/ Angular Good knowledge of HTML, HTML5, CSS, BOOTSTRAP, JAVASCRIPT & JQUERY."},
+     {profile:"Full Stack Developer",company:" Snaphunt", experience:" 1 - 3 years",salary:" ₹ 15L",location:"Bangalore/Bengaluru, Remote",discription:"In-depth knowledge of Reactjs / Vue.JS/ Angular Good knowledge of HTML, HTML5, CSS, BOOTSTRAP, JAVASCRIPT & JQUERY."},
+     { profile:"Full Stack Developer",company:" eArbor", experience:"1 - 2 years",salary:" ₹ 8L",location:" Hyderabad/Secunderabad( Madhapur )",discription:"In-depth knowledge of Reactjs / Vue.JS/ Angular Good knowledge of HTML, HTML5, CSS, BOOTSTRAP, JAVASCRIPT & JQUERY."},
+     { profile:"Full Stack Developer",company:"  Appolo Computers Pvt.Ltd", experience:" 0 - 3 years",salary:" ₹ 10L",location:"New Delhi",discription:"In-depth knowledge of Reactjs / Vue.JS/ Angular Good knowledge of HTML, HTML5, CSS, BOOTSTRAP, JAVASCRIPT & JQUERY."},
+     { profile:"Full Stack Developer",company:" Mindskill Hr Consultant", experience:" 1 - 4 years",salary:"₹ 18L",location:"Pune, Mumbai (All Areas) (WFH during Covid)",discription:"In-depth knowledge of Reactjs / Vue.JS/ Angular Good knowledge of HTML, HTML5, CSS, BOOTSTRAP, JAVASCRIPT & JQUERY."},
+     
+    // Product Manager
+    {profile:"Product Manager",company:"Amazon", experience:" 1 - 3 years",salary:" ₹ 20L",location:"Bangalore/Bengaluru, Remote",discription:"In-depth knowledge of Reactjs / Vue.JS/ Angular Good knowledge of HTML, HTML5, CSS, BOOTSTRAP, JAVASCRIPT & JQUERY."},
+     { profile:"Product Manager",company:"viral mafia - Digital Marketing Agency", experience:" 1 - 3 years",salary:" ₹ 8L",location:"Bangalore/Bengaluru, Remote",discription:"End to end campaign management (executing,monitoring,evaluating,and optimizing campaign performance to generate maximum leads"},
+     { profile:"Product Manager",company:"Techizer Tech Solutions Private Limited", experience:"1 - 5 years",salary:" ₹ 10L",location:"Mumbai, Borivali",discription:"End to end campaign management (executing,monitoring,evaluating,and optimizing campaign performance to generate maximum leads"},
+     { profile:"Product Manager",company:"Aarrrmor Digital", experience:"1 - 5 years",salary:" ₹ 18L",location:"Hyderabad/Secunderabad, Chennai, Bangalore/Bengaluru",discription:"End to end campaign management (executing,monitoring,evaluating,and optimizing campaign performance to generate maximum leads"},
+     { profile:"Product Manager",company:" Xenia Consulting", experience:" 0 - 1 years",salary:" ₹ 20L",location:"Pune( Bavdhan ) (WFH during Covid)",discription:"End to end campaign management (executing,monitoring,evaluating,and optimizing campaign performance to generate maximum leads"},
+     { profile:"Product Manager",company:"KMR Living", experience:"1 - 5 years",salary:" ₹ 8L",location:"New Delhi( New Friends Colony )",discription:"End to end campaign management (executing,monitoring,evaluating,and optimizing campaign performance to generate maximum leads"},
+   
+    
+    { profile:"Quality Engineer",company:"Meta", experience:"1 - 2 years",salary:" ₹ 20L",location:" Hyderabad/Secunderabad( Madhapur )",discription:"In-depth knowledge of Reactjs / Vue.JS/ Angular Good knowledge of HTML, HTML5, CSS, BOOTSTRAP, JAVASCRIPT & JQUERY."},
+    { profile:"Software Development Engineer-I",company:"Apple", experience:" 0 - 3 years",salary:" ₹ 10L",location:"New Delhi",discription:"In-depth knowledge of Reactjs / Vue.JS/ Angular Good knowledge of HTML, HTML5, CSS, BOOTSTRAP, JAVASCRIPT & JQUERY."},
+    { profile:"Design Engineer",company:" TATA Motors", experience:" 1 - 4 years",salary:"₹ 8L",location:"Pune, Mumbai (All Areas) (WFH during Covid)",discription:"In-depth knowledge of Reactjs / Vue.JS/ Angular Good knowledge of HTML, HTML5, CSS, BOOTSTRAP, JAVASCRIPT & JQUERY."},
+    
+   
+     
+      // // //Digital Marketing
+      { profile:"Digital Marketing Specialists",company:"viral mafia - Digital Marketing Agency", experience:" 1 - 3 years",salary:" ₹ 18L",location:"Bangalore/Bengaluru, Remote",discription:"End to end campaign management (executing,monitoring,evaluating,and optimizing campaign performance to generate maximum leads"},
+      { profile:"Digital Marketing Executive",company:"Techizer Tech Solutions Private Limited", experience:"1 - 5 years",salary:" ₹ 15L",location:"Mumbai, Borivali",discription:"End to end campaign management (executing,monitoring,evaluating,and optimizing campaign performance to generate maximum leads"},
+      { profile:"Digital Marketing Manager",company:"Aarrrmor Digital", experience:"1 - 5 years",salary:" ₹ 10L",location:"Hyderabad/Secunderabad, Chennai, Bangalore/Bengaluru",discription:"End to end campaign management (executing,monitoring,evaluating,and optimizing campaign performance to generate maximum leads"},
+      { profile:" Jr. Digital Marketing Executive",company:" Xenia Consulting", experience:" 0 - 1 years",salary:" ₹ 8L",location:"Pune( Bavdhan ) (WFH during Covid)",discription:"End to end campaign management (executing,monitoring,evaluating,and optimizing campaign performance to generate maximum leads"},
+      { profile:"Digital Marketing Specialists",company:"KMR Living", experience:"1 - 5 years",salary:" ₹ 20L",location:"New Delhi( New Friends Colony )",discription:"End to end campaign management (executing,monitoring,evaluating,and optimizing campaign performance to generate maximum leads"},
+  
+      
+    // Systems Engineer
+    { profile:"Systems Engineer",company:"Azeus Systems Limited", experience:"1 - 5 years",salary:" ₹ 20L","location":"Delhi ",discription:"Knowledge or experience in AWS deployment and Azure deployments."},
+    { profile:"System Engineer",company:"Talent Zone Consultant", experience:"1 - 3 years",salary:" ₹ 15L","location":"Mumbai",discription:"Knowledge or experience in AWS deployment and Azure deployments."},
+    { profile:"System Engineer",company:"Flipkart", experience:"1 - 5 years",salary:" ₹ 21L","location":"Bangalore/Bengaluru",discription:"Knowledge or experience in AWS deployment and Azure deployments."},
+    { profile:"System Engineer",company:"3i Infotech", experience:"0 - 5 years",salary:" ₹ 10L","location":"Hyderabad/Secunderabad",discription:"Knowledge or experience in AWS deployment and Azure deployments."},
+    { profile:"System Engineer",company:"Hexaware", experience:"1 - 5 years",salary:"  ₹ 18L","location":"Pune",discription:"Knowledge or experience in AWS deployment and Azure deployments."},
+    // Graphics Designer
+    { profile:"Graphics Designer",company:"Digital Xpression", experience:"1 - 5 years",salary:" ₹ 18L",location:"Pune( Dhanori ), Dhanori,pune",discription:"Working knowledge of CSS3, HTML5, and JavaScript"},
+    { profile:"Graphic Designer",company:"ASSOCIATED BROADCASTING COMPANY PRIVATE LIMITED",experience:"0 - 5 years",salary:" ₹ 8L",location:"Bangalore/Bengaluru",discription:"Working knowledge of CSS3, HTML5, and JavaScript"},
+    { profile:"Graphic Designer",company:"DelveInsight Business Research", experience:"1 - 3 years",salary:" ₹ 15L",location:" Delhi / NCR",discription:"Working knowledge of CSS3, HTML5, and JavaScript"},
+    { profile:"Graphic Designer",company:"Perfex Technologies", experience:"1 - 3 years",salary:" ₹ 21L",location:"Hyderabad/Secunderabad",discription:"Working knowledge of CSS3, HTML5, and JavaScript"},
+    { profile:"Graphic Designer",company:"Sanghvi Realty", experience:"0 - 3 years",salary:" ₹ 20L",location:"Mumbai (All Areas)",discription:"Working knowledge of CSS3, HTML5, and JavaScript"},
+  
+    // Quality Analyst
+    { profile:"Quality Analyst (Manual Tester)",company:"Teamlease Servcies limited", experience:"0 - 2 years",salary:" ₹ 18L",location:"Mumbai (All Areas) (WFH during Covid)",discription:"The QA Analyst will be responsible for supporting and coordinating all aspects of QA activities of Campaign Process."},
+    { profile:"Quality Analyst",company:"IndiaLends", experience:"2 - 4 years",salary:" ₹ 15L",location:"New Delhi( New Friends Colony )",discription:"The QA Analyst will be responsible for supporting and coordinating all aspects of QA activities of Campaign Process."},
+    { profile:"Quality Analyst",company:"Dreamsoft4u Pvt. Ltd.", experience:" 1 - 3 years",salary:" ₹ 21L",location:"Pune",discription:"The QA Analyst will be responsible for supporting and coordinating all aspects of QA activities of Campaign Process."},
+    {profile:"Quality Analyst",company:"Rightpoint India Digital Pvt. Ltd.", experience:"2 - 5 years",salary:" ₹ 20L",location:"Remote (Hiring office located in Bangalore/Bengaluru)",discription:"The QA Analyst will be responsible for supporting and coordinating all aspects of QA activities of Campaign Process."},
+    { profile:"Quality Analyst",company:"Technovert", experience:"1 - 4 years",salary:" ₹ 8L",location:"Hyderabad/Secunderabad", discription:"The QA Analyst will be responsible for supporting and coordinating all aspects of QA activities of Campaign Process."},
+    
+  ]
+localStorage.setItem("jobDetail",JSON.stringify(jobDataArr));
+// localStorage.setItem("Newjob",JSON.stringify("abcde"))
---
+++ JobPage/savedJob.js
@@ -0,0 +1,38 @@
+var newdata=JSON.parse(localStorage.getItem("savedJob"))||[];
+ console.log(newdata);
+
+//var saveJobarr= localStorage.setItem("savedJob",JSON.stringify(data))||[];
+
+//console.log(saveJobarr)
+
+
+
+    //document.querySelector("#jobs").innerHTML="";
+
+    newdata.map(function (elem){
+        var innerdiv= document.createElement("div");
+        innerdiv.setAttribute("class","innerdiv");
+        var p=document.createElement("h3");
+        p.innerText=elem.profile;
+        // console.log(p)
+        p.setAttribute("class","title")
+       
+        var c= document.createElement("p");
+        c.innerText=elem.company;
+        //console.log(c);
+        var e= document.createElement("p");
+        e.innerText=elem.experience;
+        var s= document.createElement("p");
+        s.innerText=elem.salary;
+        var l= document.createElement("p");
+        l.innerText=elem.location;
+        var d= document.createElement("p");
+        d.innerText=elem.discription;
+        
+        innerdiv.append(p,c,e,s,l,d);
+       
+        document.querySelector("#jobs").append(innerdiv);
+
+    })
+
+
